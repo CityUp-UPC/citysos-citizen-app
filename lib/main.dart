58343373d8d898a7719aade1cbdf3a5e1f5cd4d3
marjorieabanto <u202118523@upc.edu.pe>
2024-05-23 22:09:13 -0500

    add home-view

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:citysos_citizen/navbar.dart';
+import 'package:citysos_citizen/views/home_view.dart';
 import 'package:flutter/material.dart';
-import 'package:citysos_citizen/navbar.dart';
+
+
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +20,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const Navbar(),
+
     );
   }
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:citysos_citizen/navbar.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,8 +12,56 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home View'),
+    return Scaffold(
+        body: Center(
+          child:
+          ButtonWithText(color: Colors.redAccent, icon: Icons.radio_button_on, label: 'Presiona en caso de emergencia', size: 200),
+
+
+    ),
+        );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+    required this.size,
+
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+  final double? size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+
+      children: [
+        ElevatedButton(onPressed: () {
+          print('apretado');
+        }, child: Icon(icon, color: color, size: size,)
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
+
 }
